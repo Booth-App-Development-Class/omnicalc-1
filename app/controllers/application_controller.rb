1b1f8fbe0eb8b3748c3980d62314fb6513164b1e
zanderz1010 <aafabel0@chicagobooth.edu>
2022-07-03 20:21:36 +0000

    Going Boating

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,13 @@ def blank_square_form
 
   render( { :template => "calculations_templates/square_form.html.erb"})
 
-
 end
 
 
+def calculate_square
+  render( { :template => "calculations_templates/square_results.html.erb"})
 
+end
 
 
 
