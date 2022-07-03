5d8b04d2eb14a650f624cde287a7ad991e774bd0
zanderz1010 <aafabel0@chicagobooth.edu>
2022-07-03 20:17:17 +0000

    Basic Square Form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
 
+
+
+def blank_square_form
+
+  render( { :template => "calculations_templates/square_form.html.erb"})
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
 end
